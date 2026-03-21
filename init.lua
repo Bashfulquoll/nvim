d42e0e5cb16205c9040f4fe79c9b15ff8b0e8038
Bashfulquoll <204565947+Bashfulquoll@users.noreply.github.com>
2026-03-21 12:47:43 +1100

    Add more keymaps and helpful config (#3)

--- init.lua
+++ init.lua
@@ -90,3 +90,37 @@ vim.diagnostic.config {
   jump = { float = true },
 }
 
+-- Binds the 'q' ley to open diagnostics into the location list.
+-- This lets you jump to reported issues using the 'q' key basically.
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc 
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- TIP: Disable arrow keys in normal mode
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+-- keybinds to make split navigation easier.
+-- use ctrl+<hjkl> to switch between windows (ctrl+w or s for horizontal split.
+-- ctrl+wv for vertical split)
+--
+--  see `:help wincmd` for a list of all window commands
+vim.keymap.set('n', '<c-h>', '<c-w><c-h>', { desc = 'move focus to the left window' })
+vim.keymap.set('n', '<c-l>', '<c-w><c-l>', { desc = 'move focus to the right window' })
+vim.keymap.set('n', '<c-j>', '<c-w><c-j>', { desc = 'move focus to the lower window' })
+vim.keymap.set('n', '<c-k>', '<c-w><c-k>', { desc = 'move focus to the upper window' })
+
+-- Highlights text when the text is yanked (copied to clipboard)
+--  See `:help vim.hl.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function() vim.hl.on_yank() end,
+})
